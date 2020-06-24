7644013d92b73dd1b6c098c84cc0d0501be29638
Khalil <49612383+khalilVer@users.noreply.github.com>
2020-06-24 01:15:36 +0100

    add new bundles FOSRestBundle  and NelmiCorsBundle and generate crud for entity produit

--- src/HuntersKingdomBundle/Controller/produitController.php
+++ src/HuntersKingdomBundle/Controller/produitController.php
@@ -2,29 +2,134 @@
 
 namespace HuntersKingdomBundle\Controller;
 
+use HuntersKingdomBundle\Entity\produit;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
 
+
+/**
+ * Produit controller.
+ *
+ * @Route("produit")
+ */
 class produitController extends Controller
 {
+    /**
+     * Lists all produit entities.
+     *
+     * @Route("/", name="produit_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        $produits = $em->getRepository('HuntersKingdomBundle:produit')->findAll();
+
+        return $this->render('produit/index.html.twig', array(
+            'produits' => $produits,
+        ));
+    }
 
     /**
-     * @Route("/product/add")
-     * @Method("POST")
+     * Creates a new produit entity.
+     *
+     * @Route("/new", name="produit_new")
+     * @Method({"GET", "POST"})
      */
-    public function addAction(Request $request)
+    public function newAction(Request $request)
     {
         //récupérer le contenu de la requête envoyé par l'outil postman
         $data = $request->getContent();
         //deserialize data: création d'un objet 'produit' à partir des données json envoyées
-        $produit = $this->get('jms_serializer') ->deserialize($data, 'HunterKingdomBundle\Entity\produit', 'json');
+        $produit = $this->get('jms_serializer') ->deserialize($data, 'HuntersKingdomBundle\Entity\produit', 'json');
         //ajout dans la base
         $em = $this->getDoctrine()->getManager();
         $em->persist($produit);
         $em->flush();
-        return new Response('produit ajouté avec succès');
+        return new View("User Added Successfully", Response::HTTP_OK);
     }
 
+    /**
+     * Finds and displays a produit entity.
+     *
+     * @Route("/{id}", name="produit_show")
+     * @Method("GET")
+     */
+    public function showAction(produit $produit)
+    {
+        $deleteForm = $this->createDeleteForm($produit);
 
+        return $this->render('produit/show.html.twig', array(
+            'produit' => $produit,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing produit entity.
+     *
+     * @Route("/{id}/edit", name="produit_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, produit $produit)
+    {
+        $deleteForm = $this->createDeleteForm($produit);
+        $editForm = $this->createForm('HuntersKingdomBundle\Form\produitType', $produit);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('produit_edit', array('id' => $produit->getId()));
+        }
+
+        return $this->render('produit/edit.html.twig', array(
+            'produit' => $produit,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a produit entity.
+     *
+     * @Route("/{id}", name="produit_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, produit $produit)
+    {
+        $form = $this->createDeleteForm($produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($produit);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('produit_index');
+    }
+
+    /**
+     * Creates a form to delete a produit entity.
+     *
+     * @param produit $produit The produit entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(produit $produit)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('produit_delete', array('id' => $produit->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
 }
---
+++ src/HuntersKingdomBundle/Form/produitType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HuntersKingdomBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class produitType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('libelle')->add('reference')->add('description')->add('categorie')->add('prix');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HuntersKingdomBundle\Entity\produit'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'hunterskingdombundle_produit';
+    }
+
+
+}
