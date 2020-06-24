072e71a8deca9e2267b04756ccee9c0019cabc7d
Khalil <49612383+khalilVer@users.noreply.github.com>
2020-06-24 22:57:23 +0100

    add web services fot entity produit

---
+++ src/HuntersKingdomBundle/Repository/produitRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Khalil
+ * Date: 24/06/2020
+ * Time: 20:11
+ */
+
+namespace HuntersKingdomBundle\Repository;
+
+
+class produitRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
