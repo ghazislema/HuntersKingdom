9873e32820e72104990b88d6c99fdb3972161e0c
ayaGharbi <gharbiaya137@gmail.com>
2020-07-26 18:08:48 +0200

    .

---
+++ src/HuntersKingdomBundle/Form/sellType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace HuntersKingdomBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class sellType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titre')->add('adresse')->add('description')->add('categorie')
+            ->add('deadline')->add('datePublication')->add('etat');
+    }/**
+ * {@inheritdoc}
+ */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HuntersKingdomBundle\Entity\sell'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'hunterskingdombundle_sell';
+    }
+
+
+}
